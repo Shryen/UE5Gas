9f3571f8563b9184bf15ab8230b24836979372b6
Shryen <hbk9238@hotmail.com>
2025-04-22 17:08:16 +0200

    Blueprint function library

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,22 @@
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Player/AuraPlayerState.h"
+#include "UI/HUD/AuraHUD.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+
+UOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if(APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if(AAuraHUD* AuraHUD = Cast<AAuraHUD>(PC->GetHUD()))
+		{
+			AAuraPlayerState* PlayerState = PC->GetPlayerState<AAuraPlayerState>();
+			UAbilitySystemComponent* ASC = PlayerState->GetAbilitySystemComponent();
+			UAttributeSet* AS = PlayerState->GetAttributeSet();
+			const FWidgetControllerParams WidgetControllerParams(PC, PlayerState, ASC, AS);
+			return AuraHUD->GetOverLayWidgetController(WidgetControllerParams);
+		}
+	}
+	return nullptr;
+}
