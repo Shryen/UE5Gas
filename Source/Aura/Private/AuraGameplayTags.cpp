d36c54ca377e2d8ad2153898288b7fb3048e05ed
Shryen <hbk9238@hotmail.com>
2025-06-03 23:20:09 +0200

    InputConfig Set Up

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -52,5 +52,20 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	FName("Attribute.Secondary.MaxHealth"), FString("Defines how much health the player can have."));
 	GameplayTags.Attributes_Secondary_MaxMana = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Attribute.Secondary.MaxMana"), FString("Defines how much mana the player can have."));
-	
+
+	/*
+	 * InputTags
+	 */
+	GameplayTags.InputTag_LMB = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.LMB"), FString("InputTag for left mouse button"));
+	GameplayTags.InputTag_RMB = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.rMB"), FString("InputTag for right mouse button"));
+	GameplayTags.InputTag_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.1"), FString("InputTag for 1 key"));
+	GameplayTags.InputTag_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.2"), FString("InputTag for 2 key"));
+	GameplayTags.InputTag_3 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.3"), FString("InputTag for 3 key"));
+	GameplayTags.InputTag_4 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.4"), FString("InputTag for 4 key"));
 }
